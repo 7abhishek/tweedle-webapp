9b7e231ebf189adf2f3501e12840f9d9c76d2667
7abhishek <abhishek@redmart.com>
2017-04-02 11:01:02 +0800

    bootstrap, buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FacebookService, FacebookLoginResponse, FacebookInitParams} from 'ng2-facebook-sdk';
+import {AuthService} from './auth.service'
+import {User} from './models/user'
+import {UserService} from './user.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title:string;
+    isUserLoggedIn:boolean;
+    userId:string;
+    userName:string;
+    userPicture:string;
+    constructor(private fb:FacebookService, private auth: AuthService, private userservice:UserService) {
+        this.title = "Tweedle";
+        console.log("this ", this);
+    }
+
+    isUserConnected(){
+        this.auth.isUserConnected().then(
+            (response) => {this.isUserLoggedIn = response.status === "connected";
+                this.userId = response.authResponse.userID;
+                console.log("isUserConnected this isUserLoggedIn ", this.isUserLoggedIn, this.userId);
+            }
+        );
+    }
+
+    saveUser(){
+        console.log("saveUser called");
+        let user:User = new User(this.userId, this.userName, "facebook", this.userPicture);
+        console.log(this.userservice.saveUserDetails(user));
+    }
+
+    ngOnInit() {
+        console.log("isUserConnected is called");
+        this.auth.isUserConnected().then(
+            (response) => {this.isUserLoggedIn = response.status === "connected";
+                this.userId = response.authResponse.userID;
+                this.auth.getUserDetails(this.userId).then(
+                    (response:any) => {
+                        console.log(response);
+                        this.userName = response.first_name;
+                        this.userPicture = response.picture.data.url;
+                        this.saveUser();
+                    }
+                )
+                    console.log("ngOnInit this isUserLoggedIn ", this.isUserLoggedIn, this.userId);}
+        );
+
+    }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule , JsonpModule} from '@angular/http';
+import { FacebookService } from 'ng2-facebook-sdk';
 
 import { AppComponent } from './app.component';
+import { FacebookLoginComponent } from './facebook-login/facebook-login.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { AuthService } from './auth.service'
+import {UserService } from './user.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FacebookLoginComponent,
+        LoadingSpinnerComponent,
+        LoadingSpinnerComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        JsonpModule
+    ],
+    providers: [FacebookService, AuthService, UserService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
